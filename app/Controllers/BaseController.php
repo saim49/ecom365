effb02a755885f8ef12a06edee10705ba762ab19
saim <saim_49@hotmail.com>
2022-05-14 17:07:18 +0500

    df

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -45,6 +45,7 @@ class BaseController extends Controller
         // Do Not Edit This Line
         parent::initController($request, $response, $logger);
             //test
+            //test2
         // Preload any models, libraries, etc, here.
 
         // E.g.: $this->session = \Config\Services::session();
